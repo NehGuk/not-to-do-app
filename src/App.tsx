ff77058e943c19870d5a768c74b850d29e013d6d
NehGuk <nurture.tumulus_0n@icloud.com>
2026-05-28 10:15:37 +0200

    Removes App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css"
-import Header from "./components/Header"
-import TodoList from "./components/TodoList"
+import Header from "./components/Header/Header"
+import TodoList from "./components/TodoList/TodoList"
 
 export default function App() {
   return (
